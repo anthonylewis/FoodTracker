b4b12f74f96a2ca116266e0af9253d25783f53ba
Anthony Lewis <me@anthonylewis.com>
2015-06-12 22:20:50 -0500

    Rating Control

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,16 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Anthony Lewis on 6/12/15.
+//  Copyright © 2015 Anthony Lewis. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    // MARK: Initialization
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
