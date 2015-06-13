82e0ba3c9bc4b3145500124d142f51fb650c18cd
Anthony Lewis <me@anthonylewis.com>
2015-06-12 22:27:13 -0500

    Red Button

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,5 +12,9 @@ class RatingControl: UIView {
     // MARK: Initialization
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.redColor()
+        addSubview(button)
     }
 }
