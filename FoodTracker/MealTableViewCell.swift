aa6246ec41e50abf297044ae97e3f758d39df0a5
Anthony Lewis <me@anthonylewis.com>
2015-06-14 19:57:38 -0500

    Table Cell UI

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Anthony Lewis on 6/14/15.
+//  Copyright © 2015 Anthony Lewis. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
