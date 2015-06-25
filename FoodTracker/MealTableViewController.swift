dd7709e65c1aeecff45827ec661e6bdea7f0b849
Anthony Lewis <me@anthonylewis.com>
2015-06-24 21:47:01 -0500

    Add method to save meals

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -136,4 +136,11 @@ class MealTableViewController: UITableViewController {
         }
     }
 
+    // MARK: NSCoding
+    func saveMeals() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile:Meal.ArchiveURL.path!)
+        if !isSuccessfulSave {
+            print("Failed to save meals...")
+        }
+    }
 }
